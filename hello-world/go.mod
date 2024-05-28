47591d20c3f51286ff85b5ba33c9f5ce095bad8f
Kanak <kanakkangdan22@gmail.com>
2024-05-28 18:30:55 +0530

    hello-world

---
+++ hello-world/go.mod
@@ -0,0 +1,5 @@
+module github.com/hello-world
+
+go 1.21.7
+
+// module is a collection of packages that are released, versioned, and distributed together.
---
+++ hello-world/hello.go
@@ -0,0 +1,11 @@
+// package: a package is a way to group functions and data together
+package main
+
+// import statement: import other packages that you need
+import "fmt"
+
+// main function: the entry point of the program
+func main() {
+	// fmt package: implements formatted I/O with functions analogous to C's printf and scanf
+	fmt.Println("Hello, World!")
+}
